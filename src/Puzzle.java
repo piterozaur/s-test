4f965d87ab09614398b0ffe3109e8b8b10ae4884
Piotr Kluch 165436 <165436@edu.p.lodz.pl>
2014-04-08 16:15:29 +0200

    Initial commit

---
+++ src/Puzzle.java
@@ -0,0 +1,33 @@
+
+public class Puzzle {
+
+	public int[] solution;
+	
+	Puzzle() {
+		
+		//Set solution
+		setSolution();
+		
+	}
+	
+	public void setSolution() {
+		
+		solution[0] = 4;
+		solution[1] = 4;
+		solution[2] = 0;
+		
+	}
+	
+	public boolean checkIfPuzzleIsSolved( int mug1, int mug2, int mug3 ) {
+		
+		if ( solution[0] == mug1 &&
+			 solution[1] == mug2 &&
+			 solution[2] == mug3
+		)
+			return true;
+		else
+			return false;
+		
+	}
+
+}
